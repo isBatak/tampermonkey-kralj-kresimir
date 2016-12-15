df657143d50022a8591295d811fc1707df66a743
Ivica Batinić <ivica.batinic@infinum.hr>
2016-12-15 12:18:17 +0100

    Init

---
+++ Kralj kresimir.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Kralj kresimir
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  shows how to use babel compiler
+// @author       You
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @match        http://www.s-1.hr/dnevni.html.php
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+/* jshint ignore:end */
+    /* jshint esnext: false */
+    /* jshint esversion: 6 */
+
+    $('.rednibroj').prepend('<input type="checkbox"/>');
+
+/* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
