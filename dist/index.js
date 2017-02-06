b1d5fd9a66ec1eabf06efe6583f81c9b382d71b7
Ivica Batinić <ivica.batinic@infinum.hr>
2017-02-06 09:27:26 +0100

    blah

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,122 @@
-var Store = require('./store');
+// ==UserScript==
+// @name         Kralj kresimir
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Simplifies ordering on 'Kralj Kresimir Catering' page
+// @author       Ivica Batinic
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @match        http://www.s-1.hr/dnevni.html.php
+// ==/UserScript==
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+var Store = function() {
+  this.state = null;
+  this.reducer = null;
+  this.listeners = [];
+};
+
+Store.prototype.dispatch = function(action) {
+  this.state = this.reducer(this.state, action);
+  this.listeners.forEach(function(listener) {
+    listener();
+  });
+};
+
+Store.prototype.createStore = function(reducer) {
+  this.reducer = reducer;
+  this.state = this.reducer(null, {});
+}
+
+Store.prototype.getState = function() {
+  return this.state;
+}
+
+Store.prototype.subscribe = function(listener) {
+  this.listeners.push(listener);
+  return function unsubscribe() {
+    var index = this.listeners.indexOf(listener);
+    this.listeners.splice(index, 1);
+  }
+};
+
+module.exports = Store;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Store = __webpack_require__(0);
 
 (function(store) {
     'use strict';
@@ -242,3 +360,6 @@ var Store = require('./store');
 
     initButtons();
 })(new Store());
+
+/***/ })
+/******/ ]);
